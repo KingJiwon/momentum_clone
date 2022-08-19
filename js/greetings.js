960b6b6a9f06391e3f6386782f3f3ff463f1987f
KingJiwon <wldnjs0401@naver.com>
2022-08-19 18:05:15 +0900

    login_display

--- js/greetings.js
+++ js/greetings.js
@@ -2,12 +2,14 @@ const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector(".greeting");
 const logOut = document.querySelector(".logout");
+const todo = document.querySelector(".todo");
 
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  todo.classList.remove(HIDDEN_CLASS);
   loginForm.classList.add(HIDDEN_CLASS);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
@@ -22,9 +24,11 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
+  todo.classList.add(HIDDEN_CLASS);
   loginForm.classList.remove(HIDDEN_CLASS);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
+  todo.classList.remove(HIDDEN_CLASS);
   paintGreetings(savedUsername);
 }
 
@@ -32,6 +36,7 @@ function logOutBtn() {
   localStorage.removeItem(USERNAME_KEY);
   loginForm.classList.remove(HIDDEN_CLASS);
   greeting.classList.add(HIDDEN_CLASS);
+  todo.classList.add(HIDDEN_CLASS);
   loginInput.value = "";
 }
 
